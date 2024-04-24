3701c87418c4cec3fd50ab54ae69fdc01b125d0b
EHAB3x <ehabmah6969@gmail.com>
2024-04-24 13:33:09 +0200

    set global styles and finish navbar component

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import logo from "../../public/logo.png"
+import toggle from "../../public/Burger.svg"
+import Image from 'next/image'
+const Navbar = () => {
+  return (
+    <nav className="flex justify-around items-center py-8">
+        <div className="logo">
+            <Image src={logo} alt="logo" />
+        </div>
+
+        <div className="links">
+            <ul className="flex gap-[15px]">
+                <li className="text-[var(--nav-text-color)] font-bold text-[16px] leading-[16px]">
+                    الرئيسية
+                </li>
+                <li className="text-[var(--nav-text-color)] font-bold text-[16px] leading-[16px]">
+                    من نحن
+                </li>
+                <li className="text-[var(--nav-text-color)] font-bold text-[16px] leading-[16px]">
+                    اتصل بنا
+                </li>
+                <li className="text-[var(--nav-text-color)] font-bold text-[16px] leading-[16px]">
+                    الدعم الفنى
+                </li>
+                <li className="text-[var(--nav-text-color)] font-bold text-[16px] leading-[16px]">
+                    أرقام التشغيل
+                </li>
+                <li className="text-[var(--nav-text-color)] font-bold text-[16px] leading-[16px]">
+                    سياسة الخصوصية
+                </li>
+            </ul>
+        </div>
+
+        <div className="toogle">
+            <Image 
+                src={toggle}
+                alt="toggle-menu" 
+                width={32}
+                height={32}
+            />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "./components/Navbar";
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +9,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" dir="rtl">
+      <body className="bg-[var(--background-color)]">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
